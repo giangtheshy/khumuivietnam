b3710a8b4e64155d34f497cfbb117c134b9c4312
giangtheshy <72980294+giangtheshy@users.noreply.github.com>
2021-03-16 20:47:44 +0700

    update title

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Meta from "../components/Meta";
 import Link from "next/link";
 
 import "../scss/Home.module.scss";
@@ -6,10 +6,11 @@ import "../scss/Home.module.scss";
 export default function Home({ name }) {
   return (
     <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Meta
+        title="HD2GWatch"
+        keywords="HD2GWatch,HD2GWatch shop, HD2GWatch team"
+        description="HD2GWatch is the best shop"
+      />
 
       <h1>Home page</h1>
       <Link href="/about">About</Link>
