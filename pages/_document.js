356117242a0a794084cb7b0fd9076957a16ba895
giangtheshy <72980294+giangtheshy@users.noreply.github.com>
2021-04-19 22:05:30 +0700

    update

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2K1B409JRR')
+        `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
