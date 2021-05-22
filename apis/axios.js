9477fd07da1124446284f566469ec6c3a7514dfd
giangtheshy <72980294+giangtheshy@users.noreply.github.com>
2021-05-22 23:17:54 +0700

    update interceptors axios instance when token expire

--- apis/axios.js
+++ apis/axios.js
@@ -1,5 +1,29 @@
 import axios from "axios";
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === "production" ? "https://khumuivietnam.herokuapp.com/" : "http://localhost:5000/",
 });
+
+axiosInstance.interceptors.response.use(
+  (response) =>
+    new Promise((resolve, reject) => {
+      resolve(response);
+    }),
+  (error) => {
+    if (!error.response) {
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
+    }
+    if (error.response.status === 403) {
+      localStorage.removeItem("firstLogin");
+      window.location = "/tai-khoan/dang-nhap";
+    } else {
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
+    }
+  }
+);
+
+export default axiosInstance;
